5401c336a9304e5749241dee750c0c491d401e11
Sam Doidge <sam@samdoidge.com>
2014-09-24 02:47:28 +0100

    current variables taken from my site..

--- simple-sticky.js
+++ simple-sticky.js
@@ -2,14 +2,14 @@ $(document).ready(function()
 {
     $(window).scroll(function(){
 
-        var offset = 160 // set offset here
+        var offset = 160; // set offset here
 
         var posTop = $(window).scrollTop() - $('.container').offset().top
-        if (posTop > 160)
+        if (posTop > offset)
         {
            $('.social-buttons').addClass('fixed');
        }
-       else if (posTop < 160)
+       else if (posTop < offset)
        {
            $('.social-buttons').removeClass('fixed');
        }
